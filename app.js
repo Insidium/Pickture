5da7d0cf21eff57199a8920c434d0bb30c888798
Insidium <ryanchallen@outlook.com>
2020-05-20 09:30:11 +1000

    Added event listeners for input changes and form submission, add event to change input based on input, created search functionality which queries API and appends results

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const auth = '563492ad6f917000010000019ea56715503f4623b75c0d96160a233f';
 const gallery = document.querySelector('.gallery');
 const searchInput = document.querySelector('.search-input');
-const submitBtn = document.querySelector('.submit-btn');
+const form = document.querySelector('.search-form');
 let searchValue;
+
+//Event Listeners
+
+//listen for change to input based on entered value
+searchInput.addEventListener('input', updateInput);
+//listen for form submit and search for the value, running searchPhotos
+form.addEventListener('submit', (e) => {
+	//prevent form from clearing the value on refresh, causing network error
+	e.preventDefault();
+	searchPhotos(searchValue);
+});
+
+//log event under searchValue variable as the entered text in the input
+function updateInput(e) {
+	searchValue = e.target.value;
+}
 
 //async function for fetching curated images to display on load
 async function curatedPhotos() {
@@ -25,6 +41,32 @@ async function curatedPhotos() {
 		//add innerHTML to the div to display as the img itself and the name of the photographer
 		galleryImg.innerHTML = `<img src='${photo.src.large}'></img>
     <p>${photo.photographer}</p>`;
+		//add the complete div to the gallery section
+		gallery.appendChild(galleryImg);
+	});
+}
+
+//async function for searching for photos, which takes in the query and:
+async function searchPhotos(query) {
+	//fetch results based on the query input
+	const dataFetch = await fetch(
+		`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page1`,
+		{
+			method: 'GET',
+			headers: { Accept: 'application/json', Authorization: auth },
+		}
+	);
+	//save the data as json under data
+	const data = await dataFetch.json();
+	//loop over array of curated photos and for each photo:
+	data.photos.forEach((photo) => {
+		//create a div to house it
+		const galleryImg = document.createElement('div');
+		//add the class of 'gallery-img' for styling
+		galleryImg.classList.add('.gallery-img');
+		//add innerHTML to the div to display as the img itself and the name of the photographer
+		galleryImg.innerHTML = `<img src='${photo.src.large}'></img>
+  <p>${photo.photographer}</p>`;
 		//add the complete div to the gallery section
 		gallery.appendChild(galleryImg);
 	});
